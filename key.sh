9631982536f2eb238ad24d9260f12df78cbcb9db
cerberus9116 <98425275+cerberus9116@users.noreply.github.com>
2022-12-03 08:44:42 -0300

    Update key.sh

--- key.sh
+++ key.sh
@@ -2,8 +2,8 @@
 SCPdir="/etc/cgh" && [[ ! -d ${SCPdir} ]] && mkdir  ${SCPdir}
 cd /etc/cgh
 rm -f manager*
-#wget -q https://raw.githubusercontent.com/ChumoGH/ChumoGH-Script/master/manager
+#wget -q https://raw.githubusercontent.com/cerberus9116/Chu/master/manager
 #cat manager> /bin/gerar
-echo 'source <(curl -sL https://raw.githubusercontent.com/ChumoGH/ChumoGH-Script/master/manager)' > /bin/gerar
+echo 'source <(curl -sL https://raw.githubusercontent.com/cerberus9116/Chu/master/manager)' > /bin/gerar
 chmod +x /bin/gerar
 rm -f key.*
